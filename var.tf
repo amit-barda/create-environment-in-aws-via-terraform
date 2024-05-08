0d64011c2955e4ec7bf462b4068e85a2d20249b5
amit-barda <127838879+amit-barda@users.noreply.github.com>
2024-05-08 10:12:27 +0300

    Update var.tf

--- var.tf
+++ var.tf
@@ -7,23 +7,23 @@ variable "region" {
 # Variable: access_key - Specifies the AWS access key
 variable "access_key" {
   type        = string
-  default     = "put here ur shit"
+  default     = "your_key"
 }
 
 # Variable: secret_key - Specifies the AWS secret key
 variable "secret_key" {
   type        = string
-  default     = "some is here like abuv"
+  default     = "your_key"
 }
 
 # Variable: s3name - Specifies the name of the S3 bucket
 variable "s3name" {
   type        = string
-  default     = "fromterraformforwebsiteprojcet2024"
+  default     = "your_custom_name_for_bucket"
 }
  
 # Variable: ami_id - Specifies the ID of the AMI
 variable "ami_id" {
   type        = string
-  default     = "ami-080e1f13689e07408"
+  default     = "ami-080e1f13689e07408" # exemple for ami image 
 }
